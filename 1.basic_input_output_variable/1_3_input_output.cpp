c8bbdfac5a06b6e1e4886a08e4ac8c55fff0b7cf
Leo <stu002031@yahoo.com.tw>
2022-08-20 13:51:08 +0800

    input_output

---
+++ 1.basic_input_output_variable/1_3_input_output.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <windows.h>   
+#include <stdlib.h>
+
+int main(){     
+    
+    int num;
+
+    scanf("%d",&num);    // %d interge
+    printf("%d\n",num);
+
+    //Sleep(1000);       //delay 1000ms
+    system("pause");
+
+    return 0;   
+}
+
