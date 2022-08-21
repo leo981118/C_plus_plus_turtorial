df77166e49193cf77866693fe8eef33f909db373
Leo <stu002031@yahoo.com.tw>
2022-08-21 19:29:47 +0800

    5_2_Dynamic_Array

---
+++ 5.pointer/5_2_dynamoc_array.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+
+int main(){     
+    int n; // n numbers user defined
+    int* pointer;
+    int sum = 0;
+
+    printf("please input numbers of score : ");
+    scanf("%d", &n);
+   
+    pointer=(int* )malloc(sizeof(int) * n); // malloc will give the requested address continue list to pointer, pointer+1, pointer+2... 
+                                            
+    for (int i = 0; i<n; i++){
+        printf("pointer+%d: %d\n", i, pointer + i);
+    }
+
+    for (int i = 0; i<n; i++){
+        printf("&(pointer + %d): %d\n", i, &pointer + i);
+    }
+
+    for (int i = 0; i < n; i++){
+        printf("please input %d-th score: ", i+1);
+        scanf("%d", pointer + i); // pointer[i]  =>  *(pointer + i)
+                                  //&pointer[i]  => &*(pointer + i) = pointer +i
+    }
+
+    for (int i = 0; i < n; i++){
+        sum=sum + *(pointer + i);
+    }
+
+    printf("average: %f: ", (double)sum/n);
+
+    system("pause");
+    return 0;   
+}
