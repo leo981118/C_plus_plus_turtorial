6679ac49e9dc98da2216a74391107f655672abdd
Leo <stu002031@yahoo.com.tw>
2022-08-22 16:15:53 +0800

    7_3_typedef_struct

---
+++ 7.struct/7_3_typedef_struct.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char name[100];
+    int age;
+    int blood;
+    int chineseScore;
+    int mathScore;
+    int englishScore;
+} Student;
+
+
+
+void printStudent(Student student){
+    char bloodTable[4][5]={"A", "B", "O", "AB"};
+    printf("student %s (%d)\n", student.name, student.age);
+    printf("blood type: %s \n", bloodTable[student.blood]);
+    printf("chinese score: %d \nmathScore: %d \nenglishScore: %d \n", student.chineseScore, student.mathScore, student.mathScore);
+}
+
+
+int main(){
+
+    Student students[2] = {
+        {
+            "Sonic", 30, 1, 80, 50, 95
+        },
+        {
+            "Tails", 28, 2, 75, 98, 92
+        },
+    };
+
+    printStudent(students[0]);
+    printStudent(students[1]);
+
+    system("pause");
+
+    return 0;
+}
