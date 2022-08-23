362696530afe16fbab009002ae5aa9b351728093
Leo <stu002031@yahoo.com.tw>
2022-08-23 20:49:28 +0800

    9_4_dynamic_array

---
+++ 9.difference_cahange_to_c_plus_plus/9_4_dynamic_array.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    int* ptr = new int(2);
+
+    cout << "ptr: " << ptr << endl;
+    cout << "ptr + 0: " << ptr + 0 << endl;
+    cout << "ptr + 1: " << ptr + 1 << endl;
+    cout << endl;
+    cout << "&ptr: " << &ptr << endl;
+    cout << "&ptr + 0: " << &ptr + 0 << endl;
+    cout << "&ptr + 0: " << &ptr + 1 << endl;
+    cout << endl;
+
+    *ptr = 200;
+    *(ptr + 1) = 300;
+
+    cout << "after *ptr = 200, *(ptr + 1) = 300" << endl;
+
+    cout << "ptr: " << ptr << endl;
+    cout << "*ptr: " << *ptr << endl;
+    cout << "*(ptr + 1): " << *(ptr + 1) << endl;
+
+    delete ptr;
+
+    system("pause");
+
+    return 0;
+}
