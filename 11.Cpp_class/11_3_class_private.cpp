2f24776a0ca514a200fb69818fe92a702e2a4b37
Leo <stu002031@yahoo.com.tw>
2022-08-24 01:33:23 +0800

    11_3_Class_Private

---
+++ 11.Cpp_class/11_3_class_private.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Square {
+    public:
+        // data (attribute, varaiable)
+
+        // manipulation (method, function)
+        void setLen(int n){
+            if (n > 0){
+                len = n;
+            }
+            else{
+                cout << "error" << endl;
+            }
+        }
+
+        int getLen(){
+            return len; 
+        }
+
+        int area(){
+            return len*len;
+        }
+    private:
+        // data (attribute, varaiable)
+        int len;
+};
+
+
+int main(){
+    Square a;
+    Square b;
+
+    a.setLen(10);
+    b.setLen(20);
+
+    cout << "length of square a = "<< a.getLen() <<  " area of square a = "<< a.area() << endl;
+    cout << "length of square b = "<< b.getLen() <<  " area of square b = "<< b.area() << endl;
+
+    system("pause");
+
+    return 0;
+}
